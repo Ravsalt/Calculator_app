fd20df436232fa67f8911e19514bdd1d57ccf855
Ravsalt <ravpogi10@gmail.com>
2024-10-12 11:56:36 +0800

    new expression

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ function calculate() {
             document.getElementById('display').value = 'I love you';
         } else if (displayValue.trim() === '1 + 1') {
             document.getElementById('display').value = 'I miss you';
+        } else if (displayValue.trim() === '1 + 2') {
+                document.getElementById('display').value = 'I miss you too';
+        }else if (displayValue.trim() === '143 + 2') {
+                document.getElementById('display').value = 'I love you too';
+        } else if (displayValue.trim() === '10 * 10') {
+            document.getElementById('display').value = 'Tangina mo';
         } else {
             document.getElementById('display').value = result;
         }
